b38e0039555909bd7b5f4bcef8b55d58613feb41
jyukipann <jyukipann@yahoo.co.jp>
2024-11-02 00:10:35 +0900

    完成

--- ddns_updater.py
+++ ddns_updater.py
@@ -1,17 +1,27 @@
 #!/bin/python3
 import os
 import requests
+import pathlib
+import subprocess
 
 CLOUDFLARE_API_URL = "https://api.cloudflare.com/client/v4/zones"
 
+env_path = pathlib.Path(__file__).parent / '.env'
+if env_path.exists():
+    with open(env_path) as f:
+        for line in f:
+            key, value = line.strip().split('=', 1)
+            os.environ[key] = value
+
 # 必要な環境変数を読み込む
+GLOBAL_API_KEY = os.getenv("CLOUDFLARE_GLOBAL_API_KEY")
 API_TOKEN = os.getenv("CLOUDFLARE_API_TOKEN")
 ZONE_ID = os.getenv("CLOUDFLARE_ZONE_ID")
 RECORD_ID = os.getenv("CLOUDFLARE_RECORD_ID")
 RECORD_ID_W = os.getenv("CLOUDFLARE_RECORD_ID_W")
 DNS_NAME = os.getenv("DNS_NAME")
 INTERVAL = int(os.getenv("UPDATE_INTERVAL", 300))  # 更新間隔（秒）
-E_MAIL = os.getenv("EMAIL")
+E_MAIL = os.getenv("E_MAIL")
 
 def get_public_ip():
     """IPアドレス取得"""
@@ -22,14 +32,32 @@ def get_public_ip():
     except requests.RequestException as e:
         print("Failed to get public IP:", e)
         return None
+    
+def get_dns_record_id(dns_name:str):
+    """Cloudflare DNSレコードID取得"""
+    url = f"{CLOUDFLARE_API_URL}/{ZONE_ID}/dns_records"
+    headers = {
+        'X-Auth-Email': E_MAIL,
+        'X-Auth-Key': GLOBAL_API_KEY,
+        'Content-Type': 'application/json',
+    }
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()["result"][0]["id"]
+    except requests.RequestException as e:
+        print("Failed to get DNS record ID:", e)
+        return None
 
 def update_dns_record(ip):
     """Cloudflare DNSレコードを更新"""
     url = f"{CLOUDFLARE_API_URL}/{ZONE_ID}/dns_records/{RECORD_ID}"
     headers = {
-        'X-Auth-Email:': E_MAIL,
+        'X-Auth-Email': E_MAIL,
+        # 'X-Auth-Key': API_TOKEN,
+        'Authorization': f"Bearer {API_TOKEN}",
         "Content-Type": "application/json",
-        'X-Auth-Key': API_TOKEN,
     }
     data = {
         "type": "A",
@@ -37,31 +65,69 @@ def update_dns_record(ip):
         "content": ip,
         "ttl": 1,
     }
+    
+    # print(url)
+    # print(headers)
+    # print(data)
+    # exit()
+    
+    results = []
     try:
         response = requests.put(url, json=data, headers=headers)
         # response.raise_for_status()
-        print(f"DNS record updated successfully {data['name']}:", response.json())
+        # print(f"DNS record updated successfully {data['name']}:", response.json())
+        results.append(response.json())
     except requests.RequestException as e:
-        print("Failed to update DNS record:", e)
+        # print("Failed to update DNS record:", e)
+        results.append(e)
     
     data['name'] = '*.'+DNS_NAME
     url = f"{CLOUDFLARE_API_URL}/{ZONE_ID}/dns_records/{RECORD_ID_W}"
     try:
         response = requests.put(url, json=data, headers=headers)
         # response.raise_for_status()
-        print(f"DNS record updated successfully {data['name']}:", response.json())
+        # print(f"DNS record updated successfully {data['name']}:", response.json())
+        results.append(response.json())
     except requests.RequestException as e:
-        print("Failed to update DNS record:", e)
+        # print("Failed to update DNS record:", e)
+        results.append(e)
+    
+    return results
+
+def get_dns_record(dns_name:str):
+    current_recorded_ip = (
+        subprocess.run(
+            [
+                'dig',
+                '@1.1.1.1',
+                '+short',
+                dns_name,
+            ], 
+            stdout=subprocess.PIPE)
+        .stdout
+        .decode()
+        .strip()
+    )
+    return current_recorded_ip
 
 def main():
-    """定期的にDDNSを更新"""
     ip = get_public_ip()
-    if ip:
-        print(f"Current IP: {ip}")
-        update_dns_record(ip)
+    current_recorded_ip = get_dns_record(DNS_NAME)
+    if ip is not None and ip != current_recorded_ip:
+        # print("Updating DNS record...")
+        result = update_dns_record(ip)
+        result = [str(r) for r in result]
+        result = '\n'.join(result)
+        subprocess.run([
+            'discord_notifier.py', 
+            'Cloudflare DDNS', 
+            f"detected mismatch between public IP and DNS record\n{ip} != {current_recorded_ip}\n{result}"
+        ])
     else:
-        print("Failed to retrieve IP address.")
+        # print("No update required")
+        pass
 
 if __name__ == "__main__":
     main()
+    # print(get_dns_record_id(DNS_NAME))
 
